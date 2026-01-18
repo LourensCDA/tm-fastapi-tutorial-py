302fadca0009ee468755586498135bc5797520d5
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2026-01-18 16:54:49 +0200

    added cors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes import issues_router
 from app.middleware.timer import timing_middleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -14,4 +15,13 @@ async def health_check():
 
 
 app.middleware("http")(timing_middleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(issues_router)
